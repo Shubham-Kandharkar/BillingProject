242c32e85dd37a30f8ff44cfa8ed534d43c08ee7
Shubham-Kandharkar <shubhamkandharkar45@gmail.com>
2023-10-23 00:07:50 +0530

    "packages added for supermaket billing application"

--- supermarket/supermarket/src/main/java/com/billing/models/DailyPriceList.java
+++ supermarket/supermarket/src/main/java/com/billing/models/DailyPriceList.java
@@ -2,7 +2,6 @@ package com.billing.models;
 
 
 import lombok.*;
-import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
